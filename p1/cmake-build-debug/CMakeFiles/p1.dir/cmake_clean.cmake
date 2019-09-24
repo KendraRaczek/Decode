8b5d3dab92f57317f2f77d0e761c02b705bc58b2
Kendra Raczek <raczek@wisc.edu>
2019-09-23 21:25:43 -0500

    Add files via upload

---
+++ p1/cmake-build-debug/CMakeFiles/p1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p1.dir/decode.c.o"
+  "p1.pdb"
+  "p1.exe"
+  "libp1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/p1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
